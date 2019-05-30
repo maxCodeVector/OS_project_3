ceacac4ccfd186e19749ad48692bbba9d91ee3f1
ColinLu50 <11610310@mail.sustech.edu.cn>
2019-05-30 14:55:23 +0800

    fail 2 with qemu

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -60,7 +60,7 @@ timer_calibrate (void)
   /* Refine the next 8 bits of loops_per_tick. */
   high_bit = loops_per_tick;
   for (test_bit = high_bit >> 1; test_bit != high_bit >> 10; test_bit >>= 1)
-    if (!too_many_loops (high_bit | test_bit))
+    if (!too_many_loops (loops_per_tick | test_bit))
       loops_per_tick |= test_bit;
 
   printf ("%'"PRIu64" loops/s.\n", (uint64_t) loops_per_tick * TIMER_FREQ);
@@ -89,17 +89,11 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
+  int64_t start = timer_ticks ();
+
   ASSERT (intr_get_level () == INTR_ON);
-  if (ticks <= 0)
-    {
-      return;
-    }
-  enum intr_level old_level = intr_disable ();
-  thread_current()->ticks = timer_ticks() + ticks;
-  list_insert_ordered(&sleep_list, &thread_current()->elem,
-		      (list_less_func *) &cmp_ticks, NULL);
-  thread_block();
-  intr_set_level(old_level);
+  while (timer_elapsed (start) < ticks) 
+    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
